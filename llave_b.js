9b9c3e2861a450d618611e2d95b16370a2eb971f
pardo133 <pardo133@hotmail.com>
2026-03-23 17:34:55 +0100

    cambio

--- llave_b.js
+++ llave_b.js
@@ -1,6 +1,8 @@
 const boton = document.getElementById("llave_b.js")
 const text = document.getElementById("status")
 boton.addEventListener("click", () => {
-    
+    text.textContent="Operacion en curso"
+    document.body.style.backgroundColor="#2c3e50"
+    console.log("Alumno B ha iniciado la operación.");
 
 })
